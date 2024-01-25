8f76eb30c70a1fdd8ed9a1959da0d6d23e2a9d2d
Caio Camargo <caiocbahia@hotmail.com>
2024-01-24 21:26:41 -0300

    exercícios Classe 01

---
+++ parte-04/ExercicioClasse01/ExercicioClasse01/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioClasse01
+{
+	internal class Person
+	{
+		public string Name;
+		public double Age;
+	}
+}
---
+++ parte-04/ExercicioClasse01/ExercicioClasse01/Program.cs
@@ -0,0 +1,28 @@
+﻿using ExercicioClasse01;
+
+Person A, B;
+
+A = new Person();
+B = new Person();
+
+Console.WriteLine("Dados da primeira pessoa:");
+
+Console.Write("Nome: ");
+A.Name = Console.ReadLine();
+Console.Write("Idade: ");
+A.Age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da segunda pessoa:");
+
+Console.Write("Nome: ");
+B.Name = Console.ReadLine();
+Console.Write("Idade: ");
+B.Age = int.Parse(Console.ReadLine());
+
+if (A.Age > B.Age)
+{
+	Console.WriteLine($"A pessoa mais velha é {A.Name}");
+} else
+{
+	Console.WriteLine($"A pessoa mais velha é {B.Name}");
+}
