2862466c8eea60a67f67115f2504a8faf8b6bfed
Caio Camargo <caiocbahia@hotmail.com>
2024-01-30 22:24:49 -0300

    exercício 1 - reuso

---
+++ parte-15/Generics-Part-01/Generics-Part-01/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Generics_Part_01.Services;
+
+PrintService printService = new PrintService(); 
+
+Console.Write("How many values?");
+//Console.ReadLine();
+
+int n = int.Parse(Console.ReadLine());
+
+
+
+for(int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);    
+}
+
+printService.Print();
+
+Console.Write("First: ");
+Console.WriteLine(printService.First());
+
+
+
+
+
