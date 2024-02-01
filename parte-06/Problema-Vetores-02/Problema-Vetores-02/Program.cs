f1356b6a314ef086273205e348418603b311cee6
Caio Camargo <caiocbahia@hotmail.com>
2024-01-31 23:28:56 -0300

    vetores - Execício 02

---
+++ parte-06/Problema-Vetores-02/Problema-Vetores-02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Console.Write("Quantos elementos terá o vetor? ");
+int n = int.Parse(Console.ReadLine());
+
+double[] vetor = new double[n];
+
+for(int i = 0; i < n; i++)
+{
+    Console.Write("Nome do item: ");
+    Console.ReadLine();
+	Console.Write("Preço do item: ");
+	vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+double sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+	sum += vetor[i];
+}
+
+double avg = sum / n;
+
+Console.WriteLine("Preço médio dos itens: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Fim do programa!");
