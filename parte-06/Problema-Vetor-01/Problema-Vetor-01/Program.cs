68556d2f38bd953306f531d40e5454ff527c2979
Caio Camargo <caiocbahia@hotmail.com>
2024-01-31 22:13:51 -0300

    vetores - problema ex 01

---
+++ parte-06/Problema-Vetor-01/Problema-Vetor-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+Console.Write("Digite o número total de pessoas da amostra: ");
+int n = int.Parse(Console.ReadLine());
+
+double[] vetor = new double[n];
+
+for(int i = 0; i < n; i++)
+{
+	vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+double sum = 0.0;
+
+for (int i = 0; i < n; i++)
+{
+	sum += vetor[i];
+}
+
+double result = sum / n;
+
+Console.WriteLine("A média das alturas das " + n + " pessoas é: " + result.ToString("F2", CultureInfo.InvariantCulture));
