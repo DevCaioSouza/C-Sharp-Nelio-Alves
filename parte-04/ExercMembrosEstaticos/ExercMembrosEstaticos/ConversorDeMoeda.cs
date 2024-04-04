0566079ae222a3c97f6128c900e2a1a9649925a6
Caio Camargo <caiocbahia@hotmail.com>
2024-04-03 23:19:12 -0300

    Exercício - membros estáticos

---
+++ parte-04/ExercMembrosEstaticos/ExercMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ExercMembrosEstaticos
+{
+	internal class ConversorDeMoeda
+	{
+		public double ValorDoDolar;
+		public double QuantosDolares;
+
+		public double PagoEmReais(double valorDoDolar, double quantosDolares)
+		{
+			return valorDoDolar * quantosDolares;
+		}
+		public double CalcularImpostos(double valorEmReais)
+		{
+			return valorEmReais * 6 / 100;
+		}
+	}
+}
---
+++ parte-04/ExercMembrosEstaticos/ExercMembrosEstaticos/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using ExercMembrosEstaticos;
+
+ConversorDeMoeda Conversor = new ConversorDeMoeda();
+
+Console.Write("Digite a cotação do dólar: ");
+Conversor.ValorDoDolar = double.Parse(Console.ReadLine());
+
+Console.Write("Quantos dólares você deseja comprar? ");
+Conversor.QuantosDolares = double.Parse(Console.ReadLine());
+
+double Resultado = Conversor.PagoEmReais(Conversor.ValorDoDolar, Conversor.QuantosDolares);
+
+Console.WriteLine("Valor a ser pago em reais pela aquisição:  " + Resultado);
+
+double Impostos = Conversor.CalcularImpostos(Resultado);
+
+Console.WriteLine("IOF descontado: " + Impostos);
+
+//Console.WriteLine(Resultado);
+
+
+
+
