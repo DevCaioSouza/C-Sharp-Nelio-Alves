a0d49a17435f8f58c83e878273b33ad8f77aefa5
Caio Camargo <caiocbahia@hotmail.com>
2024-04-04 15:18:56 -0300

    modificador PARAMS

---
+++ parte-06/ModificadorParams/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using ModificadorParams;
+
+int s1 = Calculator.Sum(1, 2, 5, 9);
+int s2 = Calculator.Sum(2, 5, 6, 8);
+
+Console.WriteLine(s1 + ", " + s2);
+
+// A ideia aqui é usar o modificador "params" no construtor de Sum, para não precisar
+// instanciar o método toda vez que for usar (seria algo como:
+// int s1 = Calculator.Sum(new int[] {1, 2, 4})
+// Na classe original teríamos o mesmo parâmetro, porém sem o "params"
+// O Params é a maneira dinâmica mais correta, torna o código mais reutilizável e menos verboso.
