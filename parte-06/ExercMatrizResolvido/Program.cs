4053117988e99dafbcd52649da08c71bcb832b0c
Caio Camargo <caiocbahia@hotmail.com>
2024-04-08 23:01:22 -0300

    estrutura básica da matriz - nested loops [i,j]

---
+++ parte-06/ExercMatrizResolvido/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Fazer um programa para ler um número inteiro N e uma matriz de
+//ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+//principal e a quantidade de valores negativos da matriz.
+
+int N = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[N, N];
+
+for (int i = 0; i < N; i++)
+{
+	string[] values = Console.ReadLine().Split(" ");
+
+	for (int j = 0; j < N; j++)
+	{
+		matriz[i, j] = int.Parse(values[j]);
+	}
+}
+
