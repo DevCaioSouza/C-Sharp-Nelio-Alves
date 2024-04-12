31199148f1e755f6fcea7ca8fa2a1cdc8408167b
Caio Camargo <caiocbahia@hotmail.com>
2024-04-11 23:13:39 -0300

    Enumerações

---
+++ parte-09/Enums-01/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_01.Entities.Enums
+{
+	enum OrderStatus : int // explicando o ":", é uma derivação de tipo. OrderStatus é derivado do tipo int
+	{
+		PendingPayment = 0,
+		Processing = 1,
+		Shipped = 2,
+		Delivered = 3
+	}
+}
---
+++ parte-09/Enums-01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Enums_01.Entities;
+using Enums_01.Entities.Enums;
+
+Order order = new Order
+{
+	Id = 1080,
+	Moment = DateTime.Now,
+	Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString(); // De enum pra string
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); // De string pra enum
+
+Console.WriteLine(txt);
+Console.WriteLine();
+Console.WriteLine(os);
