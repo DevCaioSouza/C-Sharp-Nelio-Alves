2cd3112f971674a39130e85e66dc0fe837d4411b
Caio Camargo <caiocbahia@hotmail.com>
2024-04-21 16:16:31 -0300

    Create StringBuilder class

---
+++ parte-09/ExercRes02/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using ExercRes02.Entities;
+
+Comment c1 = new Comment("Have a nice trip");
+Comment c2 = new Comment("That's awesome");
+
+Post p1 = new Post(
+	DateTime.Parse("21/06/2018 13:05:44"),
+	"Traveling to New Zealand",
+	"I'm going to visit this wonderful country",
+	12);
+
+Post p2 = new Post(
+	DateTime.Parse("28/07/2018 23:14:19"),
+	"Goodnight",
+	"See you tomorrow",
+	5);
+
+p1.AddComment(c1);
+p1.AddComment(c2);
+
+
